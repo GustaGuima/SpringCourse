5fac63e37b68471344e6c96f2b2736d8ef4947be
Gustavo Guimaraes <guga_cordeiro180@hotmail.com>
2019-08-14 14:32:08 -0300

    Padroes adicionados para inicializaçao do projeto

--- src/main/java/com/springcourse/course/SpringCourse/SpringCourseApplication.java
+++ src/main/java/com/springcourse/course/SpringCourse/SpringCourseApplication.java
@@ -1,13 +1,34 @@
 package com.springcourse.course.SpringCourse;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.springcourse.course.SpringCourse.domain.Categoria;
+import com.springcourse.course.SpringCourse.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringCourseApplication {
+public class SpringCourseApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaDAO;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseApplication.class, args);
+	}
+	 
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaDAO.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
