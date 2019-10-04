d60d8a8938481a705235675be0c74cbe0ea608fb
Gustavo <guga_cordeiro180@hotmail.com>
2019-10-04 10:02:27 -0300

    Cliente e Endereco Instanciados

--- src/main/java/com/springcourse/course/SpringCourse/SpringCourseApplication.java
+++ src/main/java/com/springcourse/course/SpringCourse/SpringCourseApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springcourse.course.SpringCourse.domain.Categoria;
 import com.springcourse.course.SpringCourse.domain.Cidade;
+import com.springcourse.course.SpringCourse.domain.Cliente;
+import com.springcourse.course.SpringCourse.domain.Endereco;
 import com.springcourse.course.SpringCourse.domain.Estado;
 import com.springcourse.course.SpringCourse.domain.Produto;
+import com.springcourse.course.SpringCourse.domain.enums.TipoCliente;
 import com.springcourse.course.SpringCourse.repositories.CategoriaRepository;
 import com.springcourse.course.SpringCourse.repositories.CidadeRepository;
+import com.springcourse.course.SpringCourse.repositories.ClienteRepository;
+import com.springcourse.course.SpringCourse.repositories.EnderecoRepository;
 import com.springcourse.course.SpringCourse.repositories.EstadoRepository;
 import com.springcourse.course.SpringCourse.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class SpringCourseApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CidadeRepository cidadeDao;
+	
+	@Autowired
+	private EnderecoRepository enderecoDao;
+	
+	@Autowired
+	private ClienteRepository clienteDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseApplication.class, args);
@@ -67,6 +78,18 @@ public class SpringCourseApplication implements CommandLineRunner{
 		cidadeDao.saveAll(Arrays.asList(cid1, cid2, cid3));
 		categoriaDAO.saveAll(Arrays.asList(cat1, cat2));
 		produtoDAO.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "33679815490", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("2735679809", "4537568943"));
+		
+		Endereco end1 = new Endereco(null, "Rua Flores", "300", "Apto 30", "Jardim", "85879320", cli1, cid1);
+		Endereco end2 = new Endereco(null, "Av Matos", "105", "Sala 800", "Centro", "85079320", cli1, cid2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(end1,end2));
+		
+		clienteDao.saveAll(Arrays.asList(cli1));
+		enderecoDao.saveAll(Arrays.asList(end1,end2));
+		
 		
 	}
 
---
+++ src/main/java/com/springcourse/course/SpringCourse/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.springcourse.course.SpringCourse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.course.SpringCourse.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	 
+}
