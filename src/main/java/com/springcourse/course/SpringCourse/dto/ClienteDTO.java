f752b09c03b0bc404b5ea509cee77d62d48d3b8d
GustaGuima <guga_cordeiro180@hotmail.com>
2020-05-14 04:50:43 -0300

    Validação Customizada: Email nao repetido na atualização de cliente

--- src/main/java/com/springcourse/course/SpringCourse/dto/ClienteDTO.java
+++ src/main/java/com/springcourse/course/SpringCourse/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.springcourse.course.SpringCourse.domain.Cliente;
+import com.springcourse.course.SpringCourse.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
